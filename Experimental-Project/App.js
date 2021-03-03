7001417f6f7c3e73289ba40c45b901708d336a57
Elisandrah <47933794+Elisandrah@users.noreply.github.com>
2021-03-03 17:34:32 -0600

    Added the ability to transition between pages
    The app can now have multiple pages. This is the basebuilding block for adding login features as well as the other pages that we will want in the app. It looks ugly right now but I'm figuring out how to make it so we can beautify it later

--- Experimental-Project/App.js
+++ Experimental-Project/App.js
@@ -1,9 +1,39 @@
 import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, Component } from 'react';
+import { Text, Button, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as GoogleSignIn from 'expo-google-sign-in';
 
-export default function App() {
+function HomeScreen({ navigation }){
+  return(
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Home Screen</Text>
+    <Button 
+      title = "Go to our old Default page"
+      onPress={() => navigation.navigate('Default')}
+      />
+  </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App(){
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Default" component={Test} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+function Test({navigation}) {
   const [fontsLoaded] = Font.useFonts({
     'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
     'Inter-SemiBoldItalic':
@@ -21,6 +51,10 @@ export default function App() {
       <Text style={{ fontFamily: "Inter-SemiBoldItalic" }}>
         We can now begin building the app from this starting point
       </Text>
+      <Button 
+      title = "Go back to the home page"
+      onPress={() => navigation.navigate('Home')}
+      />
     </View>
   );
 }
