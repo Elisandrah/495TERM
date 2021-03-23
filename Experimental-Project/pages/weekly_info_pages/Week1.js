8e178a2af5ce5e60dd7b6d41b0f511d0b064fc34
Laura Foxworth <laura.e.foxworth@gmail.com>
2021-03-22 19:36:42 -0500

    Added SectionList to Week1.js
    Changed the Week 1 info page to have individual list sections

--- Experimental-Project/pages/weekly_info_pages/Week1.js
+++ Experimental-Project/pages/weekly_info_pages/Week1.js
@@ -2,7 +2,7 @@ import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 import * as React from 'react';
 //import React, { useState, Component } from 'react';
-import { Text, Button, View, Image, StyleSheet } from 'react-native';
+import { Text, Button, View, Image, StyleSheet, StatusBar, SectionList, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
@@ -10,8 +10,26 @@ import Modal from 'react-native-modal';
 
 import { TextInput } from 'react-native-gesture-handler';
 
+const DATA = [ //This is where the week's facts will be listed
+  {
+    title: "Important Milestones",
+    data: ["Baby fact 1", "Baby fact 2", "Baby fact 3"]
+  },
+  {
+    title: "Common Symptoms",
+    data: ["Fever", "Nausea", "Dizziness"]
+  },
+  {
+    title: "Keep in Mind...",
+    data: ["Drink lots of water", "Take your vitamins", "Track your weight"]
+  },
+];
 
-
+Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 export default class CalendarScreen extends React.Component {
 /*   async componentDidMount() {
@@ -24,34 +42,36 @@ export default class CalendarScreen extends React.Component {
   render()
   { 
       return (
-          <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Week 1</Text>
-            <Text>This will be the information tab</Text>
-            <Text style={styles.bold}>Acts as an FAQ</Text>
-            <Text style={styles.italics}>
-              Questions expecting mothers may have will go here
-            </Text>
-          </View>
+        <SafeAreaView style={styles.container}>
+          <SectionList
+            sections={DATA}
+            keyExtractor={(item, index) => item + index}
+            renderItem={({ item }) => <Item title={item} />}
+            renderSectionHeader={({ section: { title } }) => (
+              <Text style={styles.header}>{title}</Text>
+            )}
+          />
+        </SafeAreaView>
     );
   }
 }
 
-const styles = StyleSheet.create
-({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16
   },
-
-  bold: {
-    fontFamily: 'Inter-Black',
-    fontSize: 20
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8
   },
-
-  italics: {
-    fontFamily: 'Inter-SemiBoldItalic',
-    fontSize: 15
+  header: {
+    fontSize: 32,
+    backgroundColor: "#fff"
+  },
+  title: {
+    fontSize: 24
   }
 });
