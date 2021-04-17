87599644973d2f69696fa52a4b2d0837188fa047
Chase Hinton <chase.hinton@gmail.com>
2021-04-16 19:05:06 -0500

    Change Calendar to horizontal CalendarList

--- Experimental-Project/pages/CalendarScreen.js
+++ Experimental-Project/pages/CalendarScreen.js
@@ -1,26 +1,35 @@
 import * as Font from 'expo-font';
 import * as React from 'react';
 import { Text, Button, View, Image, StyleSheet } from 'react-native';
-import { Calendar } from 'react-native-calendars';
+import { Calendar, CalendarList } from 'react-native-calendars';
 
 
 export default class CalendarScreen extends React.Component {
   async componentDidMount() {
      await Font.loadAsync({
-    'Inter-Black': require('../assets/fonts/Inter-Black.otf'),
-    'Inter-SemiBoldItalic':
-      'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12',
+      'Inter-Black': require('../assets/fonts/Inter-Black.otf'),
+      'Inter-SemiBoldItalic':
+        'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12',
   });
   }
   render()
   { 
+          //<Text style={styles.bold}>For scheduling</Text>
+          //<Text style={styles.italics}> 
+           // Lots of appointments as well as milestones for pregnancy 
+          //</Text>
       return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <Text style={styles.bold}>For scheduling</Text>
-          <Text style={styles.italics}> 
-            Lots of appointments as well as milestones for pregnancy 
-          </Text>
-          <Calendar />
+          <CalendarList
+            pagingEnabled={true}
+            horizontal={true}
+            onDayPress={(date) => {console.log('selected day', typeof date, date)}}
+            displayLoadingIndicator={false}
+            theme={{
+              todayTextColor: '#dc2546',
+              textDayFontWeight: '500'
+            }}
+          />
         </View>
     );
   }
@@ -37,11 +46,11 @@ const styles = StyleSheet.create
 
   bold: {
     fontFamily: 'Inter-Black',
-    fontSize: 14
+    fontSize: 12
   },
 
   italics: {
     fontFamily: 'Inter-SemiBoldItalic',
-    fontSize: 11
+    fontSize: 12
   }
 });
