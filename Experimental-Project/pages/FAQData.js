3e8170027216f81e3912e98d84be74cf7f27bcdd
Chase Hinton <chase.hinton@gmail.com>
2021-04-20 11:15:49 -0500

    Exercise FAQs

--- Experimental-Project/pages/FAQData.js
+++ Experimental-Project/pages/FAQData.js
@@ -20,6 +20,61 @@ export const QuestionDetails = [
     ]
   },
   {
+    title: "Exercise",
+    data: [
+      {
+        id: 'Exercise1',
+        question: 'Is it safe to exercise during pregnancy?',
+        answer: 'If you are healthy and your pregnancy is normal, it is safe to continue or start regular physical activity. Physical activity does not increase your risk of miscarriage, low birth weight, or early delivery. However, it is important to discuss exercise with your obstetrician or other member of your health care team during your early prenatal visits. If your health care professional gives you the OK to exercise, you can discuss what activities you can do safely.'
+      },
+      {
+        id: 'Exercise2',
+        question: 'Are there certain conditions that make exercise during pregnancy unsafe?',
+        answer: 'Women with the following conditions or pregnancy complications should not exercise during pregnancy:\n\nCertain types of heart and lung diseases\n\ncerclage\n\nBeing pregnant with twins or triplets (or more) with risk factors for preterm labor\n\nPlacenta previa after 26 weeks of pregnancy\n\nPreterm labor or ruptured membranes (your water has broken) during this pregnancy regular physical activity\n\nPreeclampsia or pregnancy-induced high blood pressure\n\nSevere anemia'
+      },
+      {
+        id: 'Exercise3',
+        question: 'What are the benefits of exercise during pregnancy?',
+        answer: 'Regular exercise during pregnancy benefits you and your fetus in these key ways:\n\nReduces back pain\n\nEases constipation\n\nMay decrease your risk of gestational diabetes, preeclampsia, and cesarean delivery\n\nPromotes healthy weight gain during pregnancy\n\nImproves your overall general fitness and strengthens your heart and blood vessels\n\nHelps you to lose the baby weight after your baby is born'
+      },
+      {
+        id: 'Exercise4',
+        question: 'How much should I exercise during pregnancy?',
+        answer: 'Ideally, pregnant women should get at least 150 minutes of moderate-intensity aerobic activity every week. An aerobic activity is one in which you move large muscles of the body (like those in the legs and arms) in a rhythmic way. Moderate intensity means you are moving enough to raise your heart rate and start sweating. You still can talk normally, but you cannot sing.\n\nExamples of moderate-intensity aerobic activity include brisk walking and general gardening (raking, weeding, or digging). You can divide the 150 minutes into 30-minute workouts on 5 days of the week or into smaller 10-minute workouts throughout each day.\n\nIf you are new to exercise, start out slowly and gradually increase your activity. Begin with as little as 5 minutes a day. Add 5 minutes each week until you can stay active for 30 minutes a day.\n\nIf you were very active before pregnancy, you can keep doing the same workouts with your obstetrician’s approval. However, if you start to lose weight, you may need to increase the number of calories that you eat.'
+      },
+      {
+        id: 'Exercise5',
+        question: 'What changes occur in the body during pregnancy that can affect my exercise routine?',
+        answer: 'Your body goes through many changes during pregnancy. It is important to choose exercises that take these changes into account:\n\nJoints—The hormones made during pregnancy cause the ligaments that support your joints to become relaxed. This makes the joints more mobile and at risk of injury. Avoid jerky, bouncy, or high-impact motions that can increase your risk of being hurt.\n\nBalance—The extra weight in the front of your body shifts your center of gravity. This places stress on joints and muscles, especially those in your pelvis and lower back. Because you are less stable and more likely to lose your balance, you are at greater risk of falling.\n\nBreathing—When you exercise, oxygen and blood flow are directed to your muscles and away from other areas of your body. While you are pregnant, your need for oxygen increases. This may affect your ability to do strenuous exercise, especially if you are overweight or obese.'
+      },
+      {
+        id: 'Exercise6',
+        question: 'What precautions should I take when exercising during pregnancy?',
+        answer: 'There are a few precautions that pregnant women should keep in mind during exercise:\n\nDrink plenty of water before, during, and after your workout. Signs of dehydration include dizziness, a racing or pounding heart, and urinating only small amounts or having urine that is dark yellow.\n\nWear a sports bra that gives lots of support to help protect your breasts. Later in pregnancy, a belly support belt may reduce discomfort while walking or running.\n\nAvoid becoming overheated, especially in the first trimester. Drink plenty of water, wear loose-fitting clothing, and exercise in a temperature-controlled room. Do not exercise outside when it is very hot or humid.\n\nAvoid standing still or lying flat on your back as much as possible. When you lie on your back, your uterus presses on a large vein that returns blood to the heart. Standing motionless can cause blood to pool in your legs and feet. These positions may cause your blood pressure to decrease for a short time.'
+      },
+      {
+        id: 'Exercise7',
+        question: 'What are some safe exercises that I can do during pregnancy?',
+        answer: 'Experts agree these exercises are safest for pregnant women:\n\nWalking—Brisk walking gives a total body workout and is easy on the joints and muscles.\n\nSwimming and water workouts—Water workouts use many of the body’s muscles. The water supports your weight so you avoid injury and muscle strain.\n\nStationary bicycling—Because your growing belly can affect your balance and make you more prone to falls, riding a standard bicycle during pregnancy can be risky. Cycling on a stationary bike is a better choice.\n\nModified yoga and modified Pilates—Yoga reduces stress, improves flexibility, and encourages stretching and focused breathing. There are prenatal yoga and Pilates classes designed for pregnant women. These classes often teach modified poses that accommodate a pregnant woman’s shifting balance. You also should avoid poses that require you to be still or lie on your back for long periods.\n\nIf you are an experienced runner, jogger, or racquet-sports player, you may be able to keep doing these activities during pregnancy. Discuss these activities with your obstetrician or other member of your health care team.'
+      },
+      {
+        id: 'Exercise8',
+        question: 'What exercises should I avoid during pregnancy?',
+        answer: 'While pregnant, avoid activities that put you at increased risk of injury, such as the following:\n\nContact sports and sports that put you at risk of getting hit in the abdomen, including ice hockey, boxing, soccer, and basketball\n\nSkydiving\n\nActivities that may result in a fall, such as downhill snow skiing, water skiing, surfing, off-road cycling, gymnastics, and horseback riding\n\n"Hot yoga” or “hot Pilates,” which may cause you to become overheated\n\nScuba diving\n\nActivities performed above 6,000 feet (if you do not already live at a high altitude)'
+      },
+      {
+        id: 'Exercise9',
+        question: 'What are warning signs that I should stop exercising?',
+        answer: 'Whether you’re a seasoned athlete or a beginner, watch for the following warning signs when you exercise. If you have any of them, stop and call your obstetrician:\n\nBleeding from the vagina\n\nFeeling dizzy or faint\n\nShortness of breath before starting exercise\n\nChest pain\n\nHeadache\n\nMuscle weakness\n\nCalf pain or swelling\n\nRegular, painful contractions of the uterus\n\nFluid gushing or leaking from the vagina'
+      },
+      {
+        id: 'Exercise10',
+        question: 'Why is it important to keep exercising after my baby is born?',
+        answer: 'Exercising after your baby is born may help improve mood and decrease the risk of deep vein thrombosis (DVT), a condition that can occur more frequently in women in the weeks after childbirth. In addition to these health benefits, exercise after pregnancy can help you lose the extra pounds that you may have gained during pregnancy.'
+      }
+    ]
+  },
+  {
     // Source: www.webmd.com/baby/guide/inducing-labor
     title: 'Labor Induction',
     data: [
@@ -31,7 +86,7 @@ export const QuestionDetails = [
       {
         id: 'Induction2',
         question: 'Why is labor induced?',
-        answer: "1 out of 4 women in the U.S. start labor with induction. Some reasons: You're 1-2 weeks past your due date. Your water breaks but labor doesn't start, raising the risk of infection. A problem puts you or your baby's health at risk; such problems include conditions like diabetes, high blood pressure, preeclampsia, or eclampsia. Some doctors recommend \"elective\" inductions for non-medical reasons. Maybe you live far from the hospital or maybe your doctor asks you to accommodate their schedule. Experts say you should reconsider though, because induction poses some risks."
+        answer: "1 out of 4 women in the U.S. start labor with induction. Some reasons: \n\nYou're 1-2 weeks past your due date.\n\nYour water breaks but labor doesn't start, raising the risk of infection.\n\nA problem puts you or your baby's health at risk. Such problems include conditions like diabetes, high blood pressure, preeclampsia, or eclampsia.\n\nSome doctors recommend \"elective\" inductions for non-medical reasons. Maybe you live far from the hospital or maybe your doctor asks you to accommodate their schedule. Experts say you should reconsider though, because induction poses some risks."
       }
     ]
   }
